f7b489c80b98af755284b9f454ae3b1aeb2da786
Elijah Kennedy <elijahclimbs@gmail.com>
2024-07-15 19:29:56 -0400

    square is moving

--- code.ts
+++ code.ts
@@ -44,46 +44,38 @@ if (figma.editorType === 'figjam') {
   // This shows the HTML page in "ui.html".
   figma.showUI(__html__);
 
-  // Calls to "parent.postMessage" from within the HTML page will trigger this
-  // callback. The callback will be passed the "pluginMessage" property of the
-  // posted message.
-  figma.ui.onmessage =  (msg: {type: string, count: number}) => {
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === 'create-shapes') {
-      const numberOfShapes = msg.count;
-      const nodes: SceneNode[] = [];
-      for (let i = 0; i < numberOfShapes; i++) {
-        const shape = figma.createShapeWithText();
-        // You can set shapeType to one of: 'SQUARE' | 'ELLIPSE' | 'ROUNDED_RECTANGLE' | 'DIAMOND' | 'TRIANGLE_UP' | 'TRIANGLE_DOWN' | 'PARALLELOGRAM_RIGHT' | 'PARALLELOGRAM_LEFT'
-        shape.shapeType = 'ROUNDED_RECTANGLE'
-        shape.x = i * (shape.width + 200);
-        shape.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-        figma.currentPage.appendChild(shape);
-        nodes.push(shape);
-      }
+  // Create a rectangle (box) on the canvas
+  const box = figma.createRectangle();
+  box.resize(100, 100);
+  box.x = figma.viewport.center.x - 50;
+  box.y = figma.viewport.center.y - 50;
 
-      for (let i = 0; i < (numberOfShapes - 1); i++) {
-        const connector = figma.createConnector();
-        connector.strokeWeight = 8
-
-        connector.connectorStart = {
-          endpointNodeId: nodes[i].id,
-          magnet: 'AUTO',
-        };
-
-        connector.connectorEnd = {
-          endpointNodeId: nodes[i+1].id,
-          magnet: 'AUTO',
-        };
-      }
-
-      figma.currentPage.selection = nodes;
-      figma.viewport.scrollAndZoomIntoView(nodes);
+  // Function to move the box
+  function moveBox(direction: string) {
+    const step = 20; // pixels to move per key press
+    switch (direction) {
+      case 'W':
+        box.y -= step;
+        break;
+      case 'A':
+        box.x -= step;
+        break;
+      case 'S':
+        box.y += step;
+        break;
+      case 'D':
+        box.x += step;
+        break;
     }
+  }
 
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
-    figma.closePlugin();
+  // Listen for key presses
+  figma.ui.onmessage = (msg) => {
+    if (msg.type === 'keypress') {
+      moveBox(msg.key);
+    }
   };
-};
+
+  // Keep the plugin running
+  // figma.closePlugin(); // Remove this line to keep the plugin active
+}
